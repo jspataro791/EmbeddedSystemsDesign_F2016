02aa92b330b5bc040b857098cbf10bfe297c61ca
John Spataro <jspataro@vt.edu>
2016-09-07 10:43:56 -0400

    moved some m1 stuff

---
+++ milestones/m1/milestone_1/firmware/milestone_one.X/build/default/production/_ext/1338673686/port_asm.o.d
@@ -0,0 +1,13 @@
+build/default/production/_ext/1338673686/port_asm.o:  \
+ ../../../../../../../../../microchip/harmony/v1_08_01/third_party/rtos/FreeRTOS/Source/portable/MPLAB/PIC32MX/port_asm.S  \
+ ../../../../../../../../../microchip/harmony/v1_08_01/third_party/rtos/FreeRTOS/Source/portable/MPLAB/PIC32MX/ISR_Support.h  \
+ ../src/system_config/default/FreeRTOSConfig.h 
+build/default/production/_ext/1338673686/port_asm.o:  \
+ ../src/system_config/default/FreeRTOSConfig.h  \
+ ../../../../../../../../../microchip/harmony/v1_08_01/third_party/rtos/FreeRTOS/Source/portable/MPLAB/PIC32MX/ISR_Support.h  \
+ /home/john/MPLAB-X/XC32_Compiler/pic32mx/include/sys/asm.h  \
+ /home/john/MPLAB-X/XC32_Compiler/pic32mx/include/cp0defs.h  \
+ /home/john/MPLAB-X/XC32_Compiler/pic32mx/include/proc/ppic32mx.h  \
+ /home/john/MPLAB-X/XC32_Compiler/pic32mx/include/proc/p32mx795f512l.h  \
+ /home/john/MPLAB-X/XC32_Compiler/pic32mx/include/xc.h  \
+ ../../../../../../../../../microchip/harmony/v1_08_01/third_party/rtos/FreeRTOS/Source/portable/MPLAB/PIC32MX/port_asm.S  \
---
+++ milestones/m1/milestone_1/firmware/milestone_one.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=milestone_one.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/milestone_one.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=milestoneone.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/milestoneone.x.tar
