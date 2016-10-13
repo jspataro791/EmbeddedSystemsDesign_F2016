bd35cc7e9567e2e56d46d26829a83a4f5a607b22
John Spataro <jspataro@vt.edu>
2016-10-12 20:42:28 -0400

    redid threading model

--- pi/routing/routing.py
+++ pi/routing/routing.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python2.7
+
 #############################################################
 #								::ROUTING SOFTWARE::
 #
@@ -7,22 +9,19 @@
 #
 #############################################################
 
+GLOBAL_DEBUG = True
 
 ####################
-#		  IMPORTS		 	 #
+#		  IMPORTS		 	#
 ####################
 
 import socket
 import time
 import os
-from threading import Thread
+import threading
 from Queue import Queue as queue
 from Queue import Empty
 from Queue import Full
-
-####################
-#		 LIB IMPORTS		 #
-####################
 
 
 ####################
@@ -37,12 +36,35 @@ AI_PORT                     = 5000
 GUI_PORT           	 = 6000
 PIXYIO_PORT      	 = 7000
 
+RVR_ACK                 = "ACK"
+RVR_ACK_TIMEOUT  = 0.30
+
+
+
+####################
+#		  GLOBALS		 #
+####################
+globalThreadingLock = threading.Lock()
+
+
+
 
 ####################
 #		  FUNCTIONS		 #
 ####################
 
-def openUDPPort(IP, PORT, IO_INTENT):
+def clearScreen():
+	os.system('cls' if os.name == 'nt' else 'clear')
+	
+	
+def dbgPrint(dtype, msg):
+	if GLOBAL_DEBUG is True:
+		globalThreadingLock.acquire()
+		print("- " + dtype + ": " + msg)
+		globalThreadingLock.release()
+		
+		
+def openUDPSocket(IP, PORT, IO_INTENT):
 
 	retry = 1
 
@@ -58,7 +80,7 @@ def openUDPPort(IP, PORT, IO_INTENT):
 			else:
 				raise IOError("Invalid IO intent for %s:%i" % (IP,PORT))
 		
-			print("Opened: " + repr(IP) + ":" + repr(PORT))
+			dbgPrint("SOCK", "Opened socket %s:%i" % (IP, PORT))
 			
 			return sock
 		except IOError as e:
@@ -68,108 +90,147 @@ def openUDPPort(IP, PORT, IO_INTENT):
 			else:
 				retry += 1
 
-def clearScreen():
-	os.system('cls' if os.name == 'nt' else 'clear')
 
 ####################
-#	    THREAD CLASSES		 #
-####################
+#		 IO OBJECTS		 #
+####################            
+     
+class RVRIOObject:
+        def __init__(self, IP, PORT, rvrID):
+        
+        	dbgPrint("INIT", "RVRIOObject ID: " + rvrID)
+        
+                self._sendAddress = (IP, PORT)
+                self._rxSock = openUDPSocket(IP, PORT, "READ")
+                self._txSock = openUDPSocket(IP, PORT+1, "WRITE")
+                
+                self._txQ = queue()
+                self._rxQ = queue()
+                
+                self._rvrID = rvrID
+                
+                self.run()
+                
+        def write(self, data):
+                self._txQ.put(data, block=True)
+                
+        def read(self):
+                try:
+                        data = self._rxQ.get(block=False)
+                except Empty:
+                        pass
+                else:
+                        return data
 
-class ioReadSocketWorker(Thread):
-	def __init__(self, socket, ioQueue, threadID):
-		Thread.__init__(self)
-		self._socket = socket
-		self._threadID = threadID
-		self._ioQueue = ioQueue
-
-		print("Starting socket read thread %s" % self._threadID)
-
+        def ioWriteWorker(self):
+        	
+        	dbgPrint("THREAD START", "ioWriteWorker() in RVRIOObject ID: %s" % self._rvrID)
+        	
+                while True:
+                        
+                        txData = self._txQ.get(block=True)
+                        self._txSock.sendto(txData, self._sendAddress)
+                   
+                        writeTime = time.time() * 1000                
+                        
+                        while True:
+                        
+                                rxData = self._rxSock.recv(65536)
+                                readTime = time.time()
+                                
+                                if rxData is not RVR_ACK:
+                                        self._rxQ.put(rxData, block = True)
+                                elif rxData is RVR_ACK and (readTime - writeTime) <= RVR_ACK_TIMEOUT:
+                                        break
+                                if (readTime - writeTime) > RVR_ACK_TIMEOUT:
+                                        self._txSock.sendto(txData, self._sendAddress)
+                                        writeTime = time.time()
+                                        
+                                        dbgPrint("WARN", "%s took longer than %i to ACK!" % (self._rvrID, RVR_ACK_TIMEOUT))
+                              
+        def ioReadWorker(self):
+                
+                dbgPrint("THREAD START", "ioReadWorker() in RVRIOObject ID: %s" % self._rvrID)
+                
+                while True:
+                        
+                        rxData = self._rxSock.recv(65536)
+                        
+                        self._rxQ.put(rxData, block=True)
+                        
 	def run(self):
-		data, addr = self._socket.recvfrom(1024)
-		if self._ioQueue.full() is not True:
-    			self._ioQueue.put(data, block=True, timeout=2)
-
-	def getThreadID(self):
-		return self._threadID
-
-
-class ioWriteSocketWorker(Thread):
-	def __init__(self, socket, ioQueue, threadID, address):
-		Thread.__init__(self)
-		self._socket = socket
-		self._threadID = threadID
-		self._ioQueue = ioQueue
-		self._address = address
+	
+		dbgPrint("INFO", "Starting threads in RVRIOObject ID: " + self._rvrID)
 		
-		print("Starting socket write thread %s" % self._threadID)
-
-	def run(self):
-		data = self._ioQueue.get(block=True)
-		self._socket.sendto(data, self._address)
- 
-	def getThreadID(self):
-		return self._threadID
-
-class routerWorker(Thread):
-	def __init__(self, ioObject, handler, threadID):
-		Thread.__init__(self)
-		self._ioObject = ioObject
-		self._handler = handler
-		self._threadID = threadID
-
-		print("Launched router worker thread %s" % self._threadID)
-
-	def run(self):
-		data = self._ioObject.read()
-		self._handler(data, self._ioObject)
-		self.join()
+		ioWriteThread = threading.Thread(target=self.ioWriteWorker)
+		ioReadThread = threading.Thread(target=self.ioReadWorker)
 		
-	def getThreadID(self):
-		return self._threadID
-
-####################
-#	    	  OBJECTS		 	 #
-####################
-
-class DuplexIOObject:
-	def __init__(self, IP, READPORT=0, WRITEPORT=0):
-		self._IP = IP
-		self._READPORT = READPORT
-		self._WRITEPORT = WRITEPORT
-
-		self._RXQ = queue()
-		self._TXQ = queue()
-
-		if self._READPORT is not 0:
-			self._readSock = openUDPPort(self._IP, self._READPORT, "READ")
-			self._rxWorkerThread = ioReadSocketWorker(self._readSock, self._RXQ, 
-															repr(self._READPORT))
-			self._rxWorkerThread.setDaemon(True)
-			self._rxWorkerThread.start()
-
-		if self._WRITEPORT is not 0:
-			self._writeSock = openUDPPort(self._IP, self._WRITEPORT, "WRITE")
-			address = (self._IP, self._WRITEPORT)
-			self._txWorkerThread = ioWriteSocketWorker(self._writeSock, self._TXQ, 
-														str(self._WRITEPORT), address)
-			self._txWorkerThread.setDaemon(True)
-			self._txWorkerThread.start()
-
+		ioWriteThread.setDaemon(True)
+		ioReadThread.setDaemon(True)
+		
+		ioWriteThread.start()
+		ioReadThread.start()
+		
+		
+class ioObject:
+	def __init__(self, IP, PORT, ID):
+		self._sendAddress = (IP, PORT)
+		
+		dbgPrint("INIT", "ioObject ID: " + ID)
+		
+		self._rxSock = openUDPSocket(IP, PORT, "READ")
+		self._txSock = openUDPSocket(IP, PORT+1, "WRITE")
+		
+		self._txQ = queue()
+		self._rxQ = queue()
+		
+		self._ID = ID
+		
+		self.run()
+		
 	def write(self, data):
-		try:
-			self._TXQ.put(data, block=True)
-		except Full:
-			pass
-
-	def read(self):
-		try:
-			data  = self._RXQ.get(block=True)
-		except Empty:
-			pass
-		else:
-			return data
+		self._txQ.put(data, block=True)
 		
-
+	def read(self):
+                try:
+                        data = self._rxQ.get(block=False)
+                except Empty:
+                        pass
+                else:
+                        return data
+                        
+        def ioWriteWorker(self):
+        	
+        	dbgPrint("THREAD START", "ioWriteWorker() in ioObject ID: %s" % self._ID)
+        	
+        	while True:
+        		txData = self._txQ.get(block=True)
+        		self._txSock.sendto(self._sendAddress)
+        
+        def ioReadWorker(self):
+        
+        	dbgPrint("THREAD START", "ioReadWorker() in ioObject ID: %s" % self._ID)
+        	
+        	while True:
+        	
+        		rxData = self._rxSock.recv(65536)
+        		self._rxQ.put(rxData, block=True)
+        		
+        		
+       	def run(self):
+       		     	
+        	dbgPrint("INFO", "Starting threads in ioObject ID: " + self._ID)
+		
+		ioWriteThread = threading.Thread(target=self.ioWriteWorker)
+		ioReadThread = threading.Thread(target=self.ioReadWorker)
+		
+		ioWriteThread.setDaemon(True)
+		ioReadThread.setDaemon(True)
+		
+		ioWriteThread.start()
+		ioReadThread.start()
+        	
+        	
 #######################################
 #	    	  			        MAIN		 		           #
 #######################################
@@ -180,16 +241,13 @@ if __name__ == "__main__":
 	clearScreen()
 	print("[ ROUTING START ]\n")
 
-	# Create IO objects
-	PACMAN 	= DuplexIOObject(UDP_IP, PACMAN_PORT, PACMAN_PORT + 1)
-	GHOST	= DuplexIOObject(UDP_IP, GHOST_PORT, GHOST_PORT + 1)
-	GUI 		= DuplexIOObject(UDP_IP, GUI_PORT, GUI_PORT + 1)
-	AI 		= DuplexIOObject(UDP_IP, AI_PORT, AI_PORT + 1)
-
-	# Launch Routing Threads
+        # Create IO objects
+        PACMAN = RVRIOObject(UDP_IP, PACMAN_PORT, "PACMAN")
+        GHOST = RVRIOObject(UDP_IP, GHOST_PORT, "GHOST")
+        GUI = ioObject(UDP_IP, GUI_PORT, "GUI")
+        AI = ioObject(UDP_IP, AI_PORT, "AI")
+        STATS = ioObject(UDP_IP, STATS_PORT, "STATS")
 	
-		
-	
-
+	#
 	
 	
