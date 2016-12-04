28b44afca8a8673b30356fd41c0e5e8ac3a3a983
John Spataro <jspataro@vt.edu>
2016-12-04 14:56:26 -0500

    moved lfa receive into function

--- pic32/rover_framework/firmware/src/tsk_lfa_decision.c
+++ pic32/rover_framework/firmware/src/tsk_lfa_decision.c
@@ -9,43 +9,23 @@ void LFA_DECISION_Initialize() {
 
 char lfaDecBuffer;
 
+char lfa_recv()
+{
+    int QRcvChk = xQueueReceive(lfa_rx_queue, &lfaDecBuffer, portMAX_DELAY);
+
+    if (QRcvChk == pdTRUE) {
+
+        return lfaDecBuffer;
+
+    } else {
+        sendGPIOError(ERR_BAD_MQ_RECV);
+    }
+}
+
 void LFA_DECISION_Tasks() {
     switch (LFA_DECISION_DATA.decState) {
 
-        case RECEIVING:
-            
-            int QRcvChk = xQueueReceive(lfa_rx_queue, &lfaDecBuffer, portMAX_DELAY);
-
-            if (QRcvChk == pdTRUE) {
-
-                LFA_DECISION_DATA.decState = PROCESSING;
-                break;
-                
-            } else {
-                sendGPIOError(ERR_BAD_MQ_RECV);
-                break;
-            }
-            
-        case PROCESSING:
-            
-            if(lfaDecBuffer && LFA_STRAIGHT)
-            {
-                /* straight */
-                xQueueSend(motor_ctrl_queue, MOTOR_CTRL_STRAIGHT, 0);
-                LFA_DECISION_DATA.decState = RECEIVING;
-            }
-            else if (lfaDecBuffer && LFA_TWOWAY)
-            {
-                /* two way intersection */
-            }
-            else if (lfaDecBuffer && LFA_LEFT)
-            {
-                /* left intersection */
-            }
-            else if (lfaDecBuffer && LFA_RIGHT)
-            {
-                /* right intersection */
-            }
+        
 
     }
 }
